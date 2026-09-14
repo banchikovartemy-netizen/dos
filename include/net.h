f168771cbef453bfb6a5445451cc6e1615feca3a
banchikovartemy-netizen <banchikovartemy@gmail.com>
2026-09-14 19:11:15 +0300

    Add network interface

---
+++ include/net.h
@@ -0,0 +1,18 @@
+#ifndef PCOS_NET_H
+#define PCOS_NET_H
+#include "types.h"
+void net_init(void);
+void net_poll(void);
+int net_ready(void);
+const char *net_driver(void);
+void net_get_mac(char out[18]);
+void net_get_ip(char out[16]);
+void net_get_gateway(char out[16]);
+void net_get_dns(char out[16]);
+void net_request_dhcp(void);
+void net_ping_gateway(void);
+u32 net_rx_packets(void);
+u32 net_tx_packets(void);
+u32 net_ping_replies(void);
+const char *net_state(void);
+#endif
