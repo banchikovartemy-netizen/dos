e0edea2abf10a830cae1ac35d487c2de367174d8
banchikovartemy-netizen <banchikovartemy@gmail.com>
2026-09-14 19:09:58 +0300

    Add core type definitions

---
+++ include/types.h
@@ -0,0 +1,17 @@
+#ifndef PCOS_TYPES_H
+#define PCOS_TYPES_H
+
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef unsigned long long u64;
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef unsigned int usize;
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+#endif
